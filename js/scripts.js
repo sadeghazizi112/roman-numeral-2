1fc465bb49c5d911af10c5cd2085160207118841
sadegh azizi <sadeghazizi112@yahoo.com>
2020-04-28 13:33:35 -0700

    numerals appear on screen now, cleaned up console logs and greyed text. Should be complete now.

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,40 @@
-// var input = $("input#number").val();
-var input = 2899;
-var romanNumeral = "";
 var hundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
 var tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
 var ones = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
+function romanCalculator(input) {
+  var romanNumeral = "";
+  while (input >= 1000) {
+    romanNumeral += "M"
+    input = input - 1000;
+  }
 
-while (input >= 1000) {
-  romanNumeral += "M"
-  input = input - 1000;
+  if (input >= 100) {
+    var output = Math.floor(input / 100);   //  rounds down so the latter digits don't infere with tens | 821 -> 8--
+    output = output - 1;        // this is factoring that arrays start from position 0, rather than 1
+    romanNumeral += hundreds[output]
+    input = input - Math.floor(input/100)*100; // rounding down to closest hundreds, 821 -> 800, substracts from the input, turns 821 -> 21
+  }
+
+  if (input >= 10) {
+    var output = Math.floor(input / 10);
+    output = output - 1;
+    romanNumeral += tens[output]
+    input = input - Math.floor(input/10)*10;
+  }
+
+  if (input >= 1) {
+    var output = input - 1;
+    romanNumeral += ones[output]
+  }
+  return romanNumeral;
 }
 
-console.log(romanNumeral);
-
-if (input >= 100) {
-  var output = Math.floor(input / 100);   //  output is to find the position in the array, based on the input
-  output = output - 1;        // this is factoring that arrays start from position 0, rather than 1
-  romanNumeral += hundreds[output]
-  input = input - Math.floor(input/100)*100;
-  console.log(input)
-}
-console.log(romanNumeral)
-
-if (input >= 10) {
-  var output = Math.floor(input / 10);   //  this was input = input / 100
-  output = output - 1;        // this was output = input - 1;
-  romanNumeral += tens[output]
-  input = input - Math.floor(input/10)*10;
-  console.log(input)
-}
-
-console.log(romanNumeral)
-
-if (input >= 1) {
-  var output = input - 1;
-  romanNumeral += ones[output]
-}
-
-console.log(romanNumeral)
-
-// -----------------------------------------------------
-
-// $(document).ready(function() {
-  //   $("form#numbers").submit(function(event) {
-    //     var input = $("input#number").val();
-    //
-    //   })
-    // })
-
-
-
-    // use an array?
-    // save and commit
-    // at step 6
-    // convert hundreds into arrays
-
-
-
-    // push stuff into a new array's position?
+$(document).ready(function() {
+  $("form#numbers").submit(function(event) {
+    event.preventDefault();
+    var input = $("input#englishnumber").val();
+    var romanFinalAnswer = romanCalculator(input);
+    $(".finalresult").append(romanFinalAnswer)
+  })
+})
